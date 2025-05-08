6d67872d6f2a209a73ba68e0582ca99698d23e71
Fabrício Lima <112064779+flima04@users.noreply.github.com>
2025-05-08 19:10:29 -0300

    finalização exercicio 2

--- aula2/exercicios/exercicio2.js
+++ aula2/exercicios/exercicio2.js
@@ -1,12 +1,13 @@
 let valor;
+let taxa;
 let desconto;
-let index;
 let valorFinal;
 
 valor = prompt("Informe o valor da compra: ")
-valor > 100 ? desconto = 0.1 : desconto = 0.05
-index = 1 - desconto
-valorFinal = valor*index
+valor > 100 ? taxa = 0.1 : taxa = 0.05
+desconto = valor * taxa
+valorFinal = valor - desconto
 
 console.log("Valor original: ", valor)
-console.log()
+console.log("Desconto aplicado: ", desconto)
+console.log("Valor final: ", valorFinal)
