27993d042b4dad47c5c09d2e660c63f6e7ac9650
Fabrício Lima <112064779+flima04@users.noreply.github.com>
2025-05-09 18:15:58 -0300

    Create exercicio 5 e 6

---
+++ aula2/exercicios/exercicio5.js
@@ -0,0 +1,3 @@
+let option = confirm("Deseja realmente excluir sua conta?")
+
+option == true ? alert("Conta excluida com sucesso.") : alert("Ação cancelada pelo usuário.")
---
+++ aula2/exercicios/exercicio6.js
@@ -0,0 +1,15 @@
+let nome;
+let peso;
+let altura;
+let option;
+let imc;
+
+nome = prompt("Qual o seu nome?");
+peso = prompt("Digite seu peso (em kg):");
+altura = prompt("Digite sua altura (em metros):");
+
+option = confirm("Seus dados:\nNome: " + nome + "\nPeso: " + peso + "\nAltura: " + altura + "\nDeseja continuar?");
+
+imc = peso / (altura * altura);
+
+option == true ? alert(nome + ", seu IMC é " + imc.toFixed(2)) : alert("Você saiu do programa.")
