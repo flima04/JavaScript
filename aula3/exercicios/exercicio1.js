9039c8dd8d3062db17b038d9a13a47f568fd37ec
Fabrício Lima <112064779+flima04@users.noreply.github.com>
2025-05-13 16:47:12 -0300

    Add files via upload

---
+++ aula3/exercicios/exercicio1.js
@@ -0,0 +1,12 @@
+let ano;
+let nasc;
+let idade;
+
+nasc = Number(prompt("Digite o ano de nascimento:"));
+ano = Number(prompt("Digite o ano atual:"));
+
+idade = ano - nasc;
+
+if (idade >= 18 ) {
+    
+}
---
+++ aula3/exercicios/exercicio5.js
@@ -0,0 +1,6 @@
+let inicial = parseInt(prompt("Valor inicial: "));
+let final = parseInt(prompt("Valor final:"));
+
+for (let contador = inicial; contador <= final; contador++) {
+    console.log(contador);
+}
