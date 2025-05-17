41a21bebdf91cbe6a33fc0e302a3a9f9b1037cab
Fabrício Lima <112064779+flima04@users.noreply.github.com>
2025-05-17 00:10:04 -0300

    Update exercicio03.js

--- aula04/exercicios/exercicio03.js
+++ aula04/exercicios/exercicio03.js
@@ -1,4 +1,20 @@
-const saudacao = function (nome) {
+const mensagemDeBoasVindas = function (name) {
     const data = new Date();
     const hora = data.getHours();
+    
+    if (hora >= 6 && hora <12) {
+        return `Bom dia, ${name}!`
+    }
+    else if (hora >= 12 && hora <18) {
+        return `Boa tarde, ${name}!`
+    }
+    else if (hora >= 18 && hora <24) {
+        return `Boa noite, ${name}!`
+    }
+    else {
+        return `Olá, ${name}!`
+    }
 }
+
+let nome = prompt('Digite seu nome: ');
+alert(mensagemDeBoasVindas(nome));
