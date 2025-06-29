8be572a61c48943aa0977a9211c221ae524cd33b
Fabrício Lima <112064779+flima04@users.noreply.github.com>
2025-06-29 00:35:29 -0300

    Update script.js

--- Projetos/projeto05 - Lista de Compras/script.js
+++ Projetos/projeto05 - Lista de Compras/script.js
@@ -8,27 +8,37 @@ const listaCompras = document.getElementById("lista-compras");
 const comprasConcluidas = document.getElementById("compras-concluidas");
 
 let idEdicao = null;
+let filtro = "todas";
 
+/* >>>> Carrega todos os intem salvol no LS */
 document.addEventListener('DOMContentLoaded', loadItems(), loadConcludedItems());
 
+/* >>>> Chama a função de add ou editar */
 btnAdd.addEventListener("click", () => {
     addOrEditItem();
 })
 
+/* >>>> Muda o valor do filtro e chama as funções de carregamento */
 btnFilter.addEventListener("click", () => {
-    
+    const filter = filterInput.value;
+    filtro = filter;
+    loadItems();
+    loadConcludedItems();
 })
 
+/* >>>> Retorna um array com todos os items salvos no LS */
 function getItemsLocalStorage() {
     const items = localStorage.getItem('items');
     return items ? JSON.parse(items) : [];
 }
 
+/* >>>> Retorna um array com todos os items concluídos salvos no LS */
 function getItemsConcludedLocalStorage() {
     const itemsConcluded = localStorage.getItem('concluded');
     return itemsConcluded ? JSON.parse(itemsConcluded) : [];
 }
 
+/* >>>> Cria ou edita um item e passa um novo array para as fuções de salvar no LS */
 function addOrEditItem() {
     const item = itemInput.value;
     const qtd = qtdInput.value;
@@ -74,38 +84,57 @@ function addOrEditItem() {
     catgInput.value = "";
 }
 
+/* >>>> Salva novo array de items NÃO concluídos no LS */
 function saveItemsLocalStorage(items) {
     localStorage.setItem("items", JSON.stringify(items));
 }
 
+/* >>>> Salva novo array de items concluídos no LS */
 function saveItemsConcludedLocalStorage(concluded) {
     localStorage.setItem("concluded", JSON.stringify(concluded));
 }
 
+/* >>>> Carrega os items do LS e passa para a função que add no DOM */
 function loadItems() {
     const items = getItemsLocalStorage();
-    console.log(items)
     if (!items.length){
         listaCompras.innerHTML = "";
         return;
     } else {
         listaCompras.innerHTML = "<h2>Itens adicionados:</h2>"
-        items.forEach(item => addItemNoDom(item));
+        if (filtro !== "todas") {
+            const itemsFiltered = items.filter(i => i.category === filtro);
+            itemsFiltered.forEach(item => addItemNoDom(item));
+        } else {
+            listaCompras.innerHTML = "<h2>Itens adicionados:</h2>"
+            items.forEach(item => addItemNoDom(item));
+        }
     }
 }
 
+/* >>>> Carrega os items concluídos do LS e passa para a função que add no DOM */
 function loadConcludedItems() {
     const concludeItems = getItemsConcludedLocalStorage();
-
+    console.log(concludeItems);
     if (!concludeItems.length) {
         comprasConcluidas.innerHTML = "";
         return;
     } else {
         comprasConcluidas.innerHTML = "<h2>Itens concluídos:</h2>";
-        concludeItems.forEach(item => addItemNoDom(item));
+
+        if (filtro != "todas") {
+            const itemsConcludedFiltered = concludeItems.filter(i => i.category === filtro);
+            console.log(itemsConcludedFiltered);
+            itemsConcludedFiltered.forEach(item => addItemNoDom(item));
+        } else {
+            concludeItems.forEach(item => addItemNoDom(item));
+            console.log("executei esse")
+        }
+        filtro = "todas";
     }
 }
 
+/* >>>> Adiciona items no DOM */
 function addItemNoDom(item) {
     const card = document.createElement("div");
     const title = document.createElement("h3");
@@ -156,6 +185,7 @@ function addItemNoDom(item) {
     })
 }
 
+/* >>>> Move itens que estão sendo concluídos, chama as funções salvar no LS e carregar no DOM*/
 function concludeItem(id) {
     if (id === idEdicao) {
         idEdicao = null;
@@ -173,6 +203,7 @@ function concludeItem(id) {
     loadItems();
 }
 
+/* >>>> Edita um item */
 function editItem(id) {
     const items = getItemsLocalStorage();
     const item = items.find(i => i.id === id);
@@ -192,6 +223,7 @@ function editItem(id) {
     idEdicao = id;
 }
 
+/* >>>> Remove um item */
 function deleteItem(item) {
     if (item.status === "concluded") {
         const concludeItems = getItemsConcludedLocalStorage();
